94b639a7ce017a22b52d0c27cdc074b5932fd319
ysk <tmdrnr96@naver.com>
2022-09-15 14:44:36 +0900

    Programmers[Lv1] coding-test

---
+++ src/programmers/Lv1/TEST_16.java
@@ -0,0 +1,48 @@
+package programmers.Lv1;
+
+import java.util.Arrays;
+
+public class TEST_16 {
+	/*
+	문제 설명
+	정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요.
+	단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요.
+	예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+	제한 조건
+	arr은 길이 1 이상인 배열입니다.
+	인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+	*/
+	public static void main(String[] args) {
+		int[] n = {4,3,2,1};
+		int[] result = solution(n);
+
+		System.out.println(Arrays.toString(result));
+	}
+
+	public static int[] solution(int[] arr) {
+		int[] answer = new int[arr.length == 1? arr.length : arr.length-1];
+		int min = 0;
+		int idx = 0;
+
+		for(int i = 0; i< arr.length; i++){
+			if(i == 0) min = arr[i];
+			if(min > arr[i]){
+				min = arr[i];
+				idx = i;
+			}
+		}
+		if(arr.length -1 != 0){
+			for(int i = 0; i < arr.length-1; i++){
+				if(arr[i] != arr[idx]){
+					answer[i] = i < idx ? arr[i] : arr[i+1];
+				}else{
+					answer[i] = arr[i+1];
+				}
+			}
+		}else{
+			answer[0] = -1;
+		}
+		return answer;
+	}
+}
